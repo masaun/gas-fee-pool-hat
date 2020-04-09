c34491420db96917098c9ba60001b0ba6b980d52
masaun <masanoriuno@gmail.com>
2020-04-09 12:03:26 +0900

    Allocate some files of rtoken-contract into /test and /scripts

--- truffle-config.js
+++ truffle-config.js
@@ -13,30 +13,25 @@ module.exports = {
     },
     kovan: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, process.env.RPC_URL_KOVAN)
+        return new HDWalletProvider(
+          mnemonic, 
+          process.env.RPC_URL_KOVAN,
+          0,    // address_index
+          10,   // num_addresses
+          true  // shareNonce
+        )
       },
-      network_id: 42, // Kovan's id
-      //gas: 4465030,
-      //gasPrice: 10000000000,
-      skipDryRun: true
+      //gas: 7017622,
+      //confirmations: 2,  // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 50,   // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, process.env.RPC_URL_ROPSTEN)
-      },
-      network_id: '3',
-      //gas: 4465030,
-      //gasPrice: 10000000000,
-      skipDryRun: true
+    coverage: {
+        host: "localhost",
+        network_id: "*",
+        port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+        gas: 0xfffffffffff, // <-- Use this high gas value
+        gasPrice: 0x01      // <-- Use this low gas price
     },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, process.env.RPC_URL_RINKEBY)
-      },
-      network_id: '4',
-      //gas: 4465030,
-      //gasPrice: 10000000000,
-      skipDryRun: true
-    }
   }
 }
