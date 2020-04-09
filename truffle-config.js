d6f6fe927661c1df18f9c0601ff46ab70c7025fb
masaun <masanoriuno@gmail.com>
2020-04-09 12:32:48 +0900

    Add 'plugins' and kovan's network_id to truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,13 @@ const mnemonic = process.env.MNEMONIC;
 
 
 module.exports = {
+  //@dev - For rtoken-contract
+  plugins: [
+      //"truffle-security",
+      "solidity-coverage",
+      "truffle-plugin-verify"
+  ],
+
   networks: {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
@@ -21,17 +28,18 @@ module.exports = {
           true  // shareNonce
         )
       },
+      network_id: 42, // Kovan's id
       //gas: 7017622,
       //confirmations: 2,  // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 50,   // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     coverage: {
-        host: "localhost",
-        network_id: "*",
-        port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-        gas: 0xfffffffffff, // <-- Use this high gas value
-        gasPrice: 0x01      // <-- Use this low gas price
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
     },
   }
 }
