977d7893756082671e1d03b5990e4efc4e148391
masaun <masanoriuno@gmail.com>
2020-04-11 21:51:22 +0900

    Increase transferred DAI from 1 to 3 when it is migrated

--- migrations/2_deploy_marketplace_registry.js
+++ migrations/2_deploy_marketplace_registry.js
@@ -10,7 +10,7 @@ const _rToken = contractAddressList["Kovan"]["rtoken-contract"]["rToken-logic"];
 const _rDai = contractAddressList["Kovan"]["rtoken-contract"]["rDAI-proxy"];  // rDAI
 const _allocationStrategy = contractAddressList["Kovan"]["rtoken-contract"]["Allocation-Strategy"];  // rDAI
 
-const depositedAmount = web3.utils.toWei("1");    // 1 DAI which is deposited in deployed contract. 
+const depositedAmount = web3.utils.toWei("3");    // 3 DAI which is deposited in deployed contract. 
 
 module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(MarketplaceRegistry, _erc20, _rToken, _rDai, _allocationStrategy);
@@ -19,6 +19,6 @@ module.exports = async function(deployer, network, accounts) {
 
     const iERC20 = await IERC20.at(_erc20);
 
-    //@dev - Transfer 1 DAI from deployer's address to contract address in advance
+    //@dev - Transfer 3 DAI from deployer's address to contract address in advance
     await iERC20.transfer(marketplaceRegistry.address, depositedAmount);
 };
