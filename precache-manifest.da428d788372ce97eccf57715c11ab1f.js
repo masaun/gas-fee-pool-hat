f4f02e12ffedaac6b50190b3025ea89cf8532705
masaun <masanoriuno@gmail.com>
2020-04-28 15:16:41 +0900

    Updates

---
+++ precache-manifest.da428d788372ce97eccf57715c11ab1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/gas-fee-pool/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/gas-fee-pool/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/gas-fee-pool/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/gas-fee-pool/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "4eb60d7e794af9071df9",
+    "url": "/gas-fee-pool/static/js/runtime~main.999b11c9.js"
+  },
+  {
+    "revision": "65cdb65f6a4e9c0e0f32",
+    "url": "/gas-fee-pool/static/js/main.cf3a7296.chunk.js"
+  },
+  {
+    "revision": "2e7fe87dbdbb1f960675",
+    "url": "/gas-fee-pool/static/js/2.b56399a8.chunk.js"
+  },
+  {
+    "revision": "65cdb65f6a4e9c0e0f32",
+    "url": "/gas-fee-pool/static/css/main.4d49694f.chunk.css"
+  },
+  {
+    "revision": "2e7fe87dbdbb1f960675",
+    "url": "/gas-fee-pool/static/css/2.f5889ef7.chunk.css"
+  },
+  {
+    "revision": "98eb41eb2ba83e4981d1ffc86aeffd6c",
+    "url": "/gas-fee-pool/index.html"
+  }
+];
